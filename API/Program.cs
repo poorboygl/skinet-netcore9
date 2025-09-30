949fcdfb833995ce6e2b39fc2d71d6a44051972a
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-09-30 15:55:35 +0700

    ch02-005 Setting up entity framework

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 var app = builder.Build();
