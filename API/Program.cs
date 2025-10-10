4e189a0dd427a9d136c6797522ae7c00bef1252c
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-10-10 21:17:30 +0700

    ch03-006 Seeding data

--- API/Program.cs
+++ API/Program.cs
@@ -20,4 +20,18 @@ var app = builder.Build();
 
 app.MapControllers();
 
+try
+{
+    using var scope = app.Services.CreateScope();
+    var service = scope.ServiceProvider;
+    var context = service.GetRequiredService<StoreContext>();
+    context.Database.Migrate();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
 app.Run();
