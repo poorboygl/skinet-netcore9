28c2ad0864393125a7db4ae1facd5beacfa9623e
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-10-19 21:13:01 +0700

    ch12-005 Creating a Cart service

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
 
@@ -18,11 +19,13 @@ builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepositor
 builder.Services.AddCors();
 builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
 {
-    var connString = builder.Configuration.GetConnectionString("Redis") 
+    var connString = builder.Configuration.GetConnectionString("Redis")
                     ?? throw new Exception("Cannot get redis connection string");
     var configuration = ConfigurationOptions.Parse(connString, true);
     return ConnectionMultiplexer.Connect(configuration);
 });
+
+builder.Services.AddSingleton<ICartService, CartService>();
 
 var app = builder.Build();
 
