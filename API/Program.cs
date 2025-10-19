02fcf67eefe3ae944fc9fe4767965cc5a30c9446
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-10-19 22:33:04 +0700

    ch14-002 Setting up identity

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Services;
@@ -26,6 +27,9 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
 });
 
 builder.Services.AddSingleton<ICartService, CartService>();
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+        .AddEntityFrameworkStores<StoreContext>();
 
 var app = builder.Build();
 
@@ -34,7 +38,9 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
 app.MapControllers();
+app.MapIdentityApi<AppUser>();
 
 try
 {
