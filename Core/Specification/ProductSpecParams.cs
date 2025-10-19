191a6060d20456e89b91c4bd3b3d3e5e79e38c0a
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-10-19 08:26:53 +0700

    ch05-007 Adding the search functionality

--- Core/Specification/ProductSpecParams.cs
+++ Core/Specification/ProductSpecParams.cs
@@ -34,6 +34,11 @@ public class ProductSpecParams
     
     public string? Sort { get; set; }
 
-
-    
+    private string? _search;
+    public string Search
+    {
+        get => _search ?? "";
+        set => _search = value.ToLower();
+    }
+      
 }
