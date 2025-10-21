06adb6848aeb6552d7dac2daa00de1b3140263c1
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-10-21 23:43:28 +0700

    ch14-007 Creating extension methods

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 using System.Security.Claims;
 using API.DTOs;
+using API.Extensions;
 using Core.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -41,11 +42,8 @@ public class AccountController(SignInManager<AppUser> signInManager) : BaseApiCo
     {
         if (User.Identity?.IsAuthenticated == false) return NoContent();
 
-        var user = await signInManager.UserManager.Users
-                            .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
-
-        if (user == null) return Unauthorized();
-
+        var user = await signInManager.UserManager.GetUserByEmail(User);
+                                        
         return Ok(new
         {
             user.FirstName,
