6d40b2b0537a5ea8e4c678ed526ad03490086046
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2025-10-30 21:04:37 +0700

    ch22-002 Setting up caching on the API

---
+++ Infrastructure/Services/ResponseCacheService.cs
@@ -0,0 +1,31 @@
+using System.Text.Json;
+using Core.Interfaces;
+using StackExchange.Redis;
+
+namespace Infrastructure.Services;
+
+public class ResponseCacheService(IConnectionMultiplexer redis) : IResponseCacheService
+{
+    private readonly IDatabase _database = redis.GetDatabase(1);
+    public async Task CacheResponseAsync(string cacheKey, object response, TimeSpan timeToLive)
+    {
+        var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        var serializedResponse = JsonSerializer.Serialize(response, options);
+
+        await _database.StringSetAsync(cacheKey, serializedResponse, timeToLive);
+    }
+
+    public async Task<string?> GetCachedResponseAsync(string cacheKey)
+    {
+        var cachedResponse = await _database.StringGetAsync(cacheKey);
+
+        if (cachedResponse.IsNullOrEmpty) return null;
+
+        return cachedResponse;
+    }
+
+    public Task RemoveCachePattern(string pattern)
+    {
+        throw new NotImplementedException();
+    }
+}
