018db0ab17d7a7acf64b92d757d764345027618d
nguyenvu-laptop <nguye@LAPTOP>
2025-10-30 11:29:50 +0700

    ch21-005 Updating the Base controller CreatePagedResult method

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -1,3 +1,5 @@
+using API.DTOs;
+using API.Extensions;
 using Core.Entities.OrderAggregate;
 using Core.Interfaces;
 using Core.Specification;
@@ -10,10 +12,21 @@ namespace API.Controllers;
 public class AdminController(IUnitOfWork unit) : BaseApiController
 {
     [HttpGet("orders")]
-    public async Task<ActionResult<Order>> GetOrders([FromQuery]OrderSpecParams specParams)
+    public async Task<ActionResult<IReadOnlyList<OrderDto>>> GetOrders([FromQuery] OrderSpecParams specParams)
     {
         var spec = new OrderSpecification(specParams);
 
-        return await createPagedResult(unit.Repository<Order>(), spec, specParams.PageIndex, specParams.PageSize);
+        return await createPagedResult(unit.Repository<Order>(), spec, specParams.PageIndex, specParams.PageSize, o => o.toDto());
+    }
+
+    [HttpGet("orders/{id:int}")]
+    public async Task<ActionResult<OrderDto>> GetOrderById(int id)
+    {
+        var spec = new OrderSpecification(id);
+        var order = await unit.Repository<Order>().GetEntityWithSpec(spec);
+
+        if (order == null) return BadRequest("No order with that id");
+
+        return order.toDto();
     }
 }
